31c796b039a401508aea6a2948f4c15186dda6ea
joe beuckman <joebeuckman@gmail.com>
2016-01-05 10:25:38 -0600

    initial commit

---
+++ eagle/atmega_frequency_counter.pro
@@ -0,0 +1,25 @@
+EAGLE AutoRouter Statistics:
+
+Job           : /Users/joebeuckman/Dropbox/CAM/eurorack/frequency_counter/eagle/atmega_frequency_counter.brd
+
+Start at      : 22:37:57 (1/3/16)
+End at        : 22:41:56 (1/3/16)
+Elapsed time  : 00:01:34
+
+Signals       :    35   RoutingGrid: 25 mil  Layers: 1
+Connections   :    99   predefined:  0 ( 0 Vias )
+
+Router memory :   48510
+
+Passname          :     Route Optimize1 Optimize2 Optimize3 Optimize4
+
+Time per pass     :  00:01:29  00:00:02  00:00:01  00:00:01  00:00:01
+Number of Ripups  :       398         0         0         0         0
+max. Level        :         7         0         0         0         0
+max. Total        :        41         0         0         0         0
+
+Routed            :        92        92        92        92        92
+Vias              :         0         0         0         0         0
+Resolution        :    92.9 %    92.9 %    92.9 %    92.9 %    92.9 %
+
+Final             : 92.9% finished
---
+++ frequency_counter/frequency_counter.ino
@@ -0,0 +1,97 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#include "FreqPeriod.h"
+
+double lfrq;
+long int pp;
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("FreqPeriod Library Test");
+
+  FreqPeriod::begin();
+
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
+//  display.setRotation(90);
+}
+
+void loop() {
+
+  pp=FreqPeriod::getPeriod();
+
+  if (pp ){
+
+    lfrq= 16000400.0 / pp;
+/*
+    Serial.print("period: ");
+    Serial.print(pp);
+    Serial.print(" 1/16us  /  frequency: ");
+
+    printDouble(lfrq,6);
+    Serial.print(" Hz");
+
+    Serial.println("  ");
+*/
+    display.clearDisplay();
+//    display.setRotation(1);
+  
+    display.setCursor(0,0);
+    display.setTextSize(4);
+    display.setTextColor(WHITE);
+    
+    if (lfrq < 10) {
+      display.print(String(lfrq, 4));
+    } else if (lfrq < 100) {
+      display.print(String(lfrq, 3));
+    } else if (lfrq < 1000) {
+      display.print(String(lfrq, 2));
+    } else if (lfrq < 10000) {
+      display.print(String(lfrq, 1));
+    } else {
+      display.print(int(lfrq + .5));
+    }
+  
+    display.display();
+  }
+
+}
+
+//***************************************************************************
+void printDouble( double val, byte precision){
+  // prints val with number of decimal places determine by precision
+  // precision is a number from 0 to 6 indicating the desired decimial places
+  // example: lcdPrintDouble( 3.1415, 2); // prints 3.14 (two decimal places)
+
+  if(val < 0.0){
+    Serial.print('-');
+    val = -val;
+  }
+
+  Serial.print (int(val));  //prints the int part
+  if( precision > 0) {
+    Serial.print("."); // print the decimal point
+    unsigned long frac;
+    unsigned long mult = 1;
+    byte padding = precision -1;
+    while(precision--)
+      mult *=10;
+
+    if(val >= 0)
+      frac = (val - int(val)) * mult;
+    else
+      frac = (int(val)- val ) * mult;
+    unsigned long frac1 = frac;
+    while( frac1 /= 10 )
+      padding--;
+    while(  padding--)
+      Serial.print("0");
+    Serial.print(frac,DEC) ;
+  }
+}
